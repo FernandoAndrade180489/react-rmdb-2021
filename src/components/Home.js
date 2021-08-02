158bc40e42cfa0436d67dd429e09c145900a4265
fernandowa <fernandow.and@gmail.com>
2021-08-02 18:23:04 -0300

    HeroImage Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // Config
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
-
+import HeroImage from "./HeroImage";
 // Hook
-import { useHomeFetch } from '../hooks/useHomeFetch';
+import { useHomeFetch } from "../hooks/useHomeFetch";
 // Image
-import NoImage from '../images/no_image.jpg';
+import NoImage from "../images/no_image.jpg";
 
-const Home = () => {    
-    const { state, loading, error } = useHomeFetch();  // Call custom hook
-    console.log(state);
+const Home = () => {
+  const { state, loading, error } = useHomeFetch(); // Call custom hook
 
-    return <div>Home Page</div>
-}
+  console.log(state);
+
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
+};
 
 export default Home;
